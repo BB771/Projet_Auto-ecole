b359357c712a4b8648b3249285a61ea747a49d24
Esteban <esteban.cazor@gmail.com>
2024-10-10 14:07:19 +0200

    acceuil2

--- src/main/java/sio/projetautoecole/HelloController.java
+++ src/main/java/sio/projetautoecole/HelloController.java
@@ -9,8 +9,21 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
 
+    @FXML
+    private AnchorPane accueil;
+    @FXML
+    private AnchorPane responsables;
+    @FXML
+    private AnchorPane moniteurs;
+    @FXML
+    private AnchorPane eleves;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    public void onHelloButtonClick(ActionEvent actionEvent) {
     }
 }
